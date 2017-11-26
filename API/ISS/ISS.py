7e2f391e1b4086e53351a6160100089ea03b4ac8
Uttar <uttar.bk@gmail.com>
2017-11-26 14:14:23 +1000

    ISS

--- API/ISS/ISS.py
+++ API/ISS/ISS.py
@@ -1,6 +1,7 @@
 import urllib.request
 import urllib.error
 import json
+import time
 
 
 def get_iss():
@@ -9,11 +10,11 @@ def get_iss():
         req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         response = urllib.request.urlopen(req).read()
         js = json.loads(response.decode('utf-8'))
-    except urllib.error.HTTPError:
-        print('Нет такого покемона =(')
-        exit()
-    lat = js['iss_position']['longitude']
-    lon = js['iss_position']['latitude']
+    except urllib.error.HTTPError or urllib.error.URLError:
+        print('Что-то поломалось =(')
+        return 'Что-то поломалось =('
+    lat = js['iss_position']['latitude']
+    lon = js['iss_position']['longitude']
     try:
 
         url = "http://maps.googleapis.com/maps/api/geocode/json?"
@@ -21,18 +22,31 @@ def get_iss():
         v = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         r = urllib.request.urlopen(v).read()
         j = json.loads(r.decode('utf-8'))
-    except urllib.error.HTTPError:
+    except urllib.error.HTTPError or urllib.error.URLError:
         print('Координаты МКС: %s:%s' % (lat, lon))
-        exit()
+        return lat, lon
 
     place = None
     if j['status'] == 'OK':
-        place = j['results'][1]['formatted_address']
+        try:
+            place = j['results'][1]['formatted_address']
+        except IndexError:
+            return lat, lon
 
     print('Координаты МКС: %s:%s' % (lat, lon))
     if place:
         print(place)
-    return lat, lon
+        return lat, lon, place
+    else:
+        return lat, lon
 
 
-get_iss()
+# get_iss()
+
+while True:
+    with open('result.txt', 'a', encoding='utf-8', errors=None) as f:
+        t = time.strftime("%d_%b_%Y_%H-%M ", time.gmtime())
+        text = t + str(get_iss()) + '\n'
+        f.write(text)
+        f.close()
+        time.sleep(60)
