4dec4d992b4f53d99c8d190017b16de9afd9a269
Uttar <uttar.bk@gmail.com>
2017-12-03 07:34:31 +0300

    3.12

--- API/football/football.py
+++ API/football/football.py
@@ -1,9 +1,11 @@
 '''
 ТОП-5 чемпионатов. Вывести по каждому чемпионату первые пять команд с наибольшим колчиством голов
 '''
-# НЕ ДОДЕЛАНО =(
+
 import urllib.request
 import json
+import urllib.error
+
 # Список самых клёвых чемпионатов
 top_five_comp = ['PL', 'EC', 'EL', 'CL', 'WC']
 
@@ -24,24 +26,39 @@ def get_league_table(href):
     return js
 
 
-competitions = get_competitions(season='2017')
+competitions = get_competitions(season='2016')
 
 c_five_comp = [c for c in competitions if c['league'] in top_five_comp]
-# c_five_comp = filter(lambda c: c['league'] in top_five_comp, competitions)
-# c_by_numberOfGames = sorted(competitions, key = lambda c: c['numberOfGames'])
+
 for c in c_five_comp:
-    print(c['_links']['leagueTable']['href'])
-    league_table = get_league_table(c['_links']['leagueTable']['href'])
-    if league_table.get('_links') is not None:
-        league_table.pop('_links')
-    all = []
-    # print(league_table['standings'])
-    for group in league_table['standings']:
-        for n in group:
-            for n in range(len(league_table['standings'][group])):
-                all.append(league_table['standings'][group][n])
-                # for team in league_table['standings'][group]:
-                #     print(team['goals'])
-    sorted_by_goals = sorted(all, key=lambda g: g['goals'])
-    top_five_by_goals = sorted_by_goals[-5:]
-    top_five_by_goals.reverse()
+    print(c['caption'])
+    try:
+        league_table = get_league_table(c['_links']['leagueTable']['href'])
+        all = []
+        if league_table.get('standing') is not None:
+            for group in range(len(league_table['standing'])):
+                all.append(league_table['standing'][group])
+
+        if league_table.get('_links') is not None:
+            league_table.pop('_links')
+        if league_table.get('standings') is not None:
+            for group in league_table['standings']:
+                for n in group:
+                    for n in range(len(league_table['standings'][group])):
+                        all.append(league_table['standings'][group][n])
+
+        sorted_by_goals = sorted(all, key=lambda g: g['goals'])
+        top_five_by_goals = sorted_by_goals[-5:]
+        top_five_by_goals.reverse()
+        if top_five_by_goals[0].get('teamName') is not None:
+            for command in range(len(top_five_by_goals)):
+                print('Команда: ' + top_five_by_goals[command]['teamName'] + ', колчество голов: ' + str(
+                    top_five_by_goals[command]['goals']))
+        else:
+            for command in range(len(top_five_by_goals)):
+                print('Команда: ' + top_five_by_goals[command]['team'] + ', колчество голов: ' + str(
+                    top_five_by_goals[command]['goals']))
+    except urllib.error.HTTPError:
+        print('Сервер не отдаёт данные (' + c['_links']['leagueTable']['href'] + ')')
+
+
